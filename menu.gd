d7d7d08e0b47bf65693e95979d5ff46a93929fd1
DasParijat <143519231+DasParijat@users.noreply.github.com>
2024-05-26 06:24:17 -0400

    Added play message alpha bobbing

--- menu.gd
+++ menu.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+var time : float
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"Play Message".material.set_shader_parameter("flash_modifier", 0.0)
+	#$"Play Message".material.set_shader_parameter("flash_modifier", 0.0)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# play_message_flash($"Play Message".self_modulate.a)
+	time += delta
+	$"Play Message".self_modulate.a = get_sine(5,0.25,0.75)
 	if Input.is_action_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://main_game.tscn")
 
@@ -17,6 +21,12 @@ func _process(delta):
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://main_game.tscn")
+	
+func get_sine(f, a, b):
+	# f = frequency
+	# a = amount
+	# b = base
+	return (sin(time * f) * a) + b
 
 	#print("flash off")
 	#$"Play Message".material.set_shader_parameter("flash_modifier", 0.0)
